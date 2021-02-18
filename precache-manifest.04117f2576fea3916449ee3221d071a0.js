5c4b0772a432faab99c64f820ffa1c52e147ca1f
Dan Escott <git@escott.dk>
2021-02-18 16:57:10 +0000

    Updates

---
+++ precache-manifest.04117f2576fea3916449ee3221d071a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c80ab2296d4c9babb3415b16baf0ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61821dbc904dce4a4c8a",
+    "url": "./static/css/main.cae467a6.chunk.css"
+  },
+  {
+    "revision": "1e41619095fd5abb97fc",
+    "url": "./static/js/2.4a21a75a.chunk.js"
+  },
+  {
+    "revision": "a8607117756eebc5734fd709698fb7ee",
+    "url": "./static/js/2.4a21a75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61821dbc904dce4a4c8a",
+    "url": "./static/js/main.3282d0e0.chunk.js"
+  },
+  {
+    "revision": "ea3cf71a55e2d4ada364",
+    "url": "./static/js/runtime-main.70999289.js"
+  }
+]);
