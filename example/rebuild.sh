43c3ed87feeeb981c70c54594fa4d7432d25cf2b
Dan Escott <git@escott.dk>
2022-11-12 19:07:10 +0000

    buikd: focus auditing on production deps

--- example/rebuild.sh
+++ example/rebuild.sh
@@ -2,6 +2,8 @@
 rm -rf node_modules
 rm package-lock.json
 
-npm i --save react react-dom react-scripts typescript @types/react @types/react-dom @types/node file:.. @fortawesome/free-solid-svg-icons
+npm i --no-audit --save react react-dom typescript @types/react @types/react-dom @types/node file:.. @fortawesome/free-solid-svg-icons
 
-npm i --save-dev @testing-library/jest-dom @testing-library/react @testing-library/user-event @types/jest @babel/plugin-syntax-object-rest-spread
+npm i --no-audit --save-dev react-scripts @testing-library/jest-dom @testing-library/react @testing-library/user-event @types/jest @babel/plugin-syntax-object-rest-spread
+
+npm audit --omit=dev
